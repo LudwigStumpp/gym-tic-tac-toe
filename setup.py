139d58391efa582f3e6a10ee0e396852f6045038
LudwigStumpp <ludwig.stumpp@web.de>
2020-05-30 22:11:13 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='gym_tictactoe',
+      description='OpenAI Gym environment for Tic Tac Toe',
+      author='Ludwig Stumpp',
+      license='MIT',
+      url='https://github.com/LudwigStumpp/gym-tic-tac-toe'
+      version='0.0.1',
+      install_requires=['gym']  # And any other dependencies foo needs
+      )
