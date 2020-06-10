f8592db6bd6a444ffae0621747078377cdc20dc9
LudwigStumpp <ludwig.stumpp@web.de>
2020-06-10 23:08:46 +0200

    more modularization

--- main.py
+++ main.py
@@ -8,8 +8,26 @@ import os.path
 import gym_tictactoe
 
 
+def create_Q(env):
+    if from_scratch:
+        return np.zeros([env.observation_space.n, int(env.action_space.n / 2)])
+    else:
+        try:
+            print('Loading Q-Table')
+            return pickle.load(open("q_table.p", "rb"))
+        except IOError:
+            print('Could not find file. Starting from scratch')
+            return np.zeros([env.observation_space.n,
+                             int(env.action_space.n / 2)])
+
+
 def opponent_random(env):
     return env.action_space.sample() % 9 + 9
+
+
+def opponent_random_better(env):
+    valid_moves = env.get_valid_moves()
+    return random.choice(valid_moves) + 9
 
 
 def opponent_human(env):
@@ -22,14 +40,14 @@ def opponent_human(env):
     return action
 
 
-def agent_move(action_space, state, Q):
-    if random.uniform(0, 1) < epsilon:
+def agent_move(action_space, state, Q, explore):
+    if explore and random.uniform(0, 1) < epsilon:
         return action_space.sample() % 9  # explore action space
     else:
         return np.argmax(Q[state])  # exploit learned values
 
 
-def play_one(env, Q, opponent, render=False, update=True, first=True):
+def play_one(env, Q, opponent, render=False, update=True, first=True, explore=True):
     action_space = env.action_space
 
     state = env.reset()
@@ -44,7 +62,7 @@ def play_one(env, Q, opponent, render=False, update=True, first=True):
     while not done:
         # Agent moves, skip in first round if second
         if first or opponent_moved:
-            action = agent_move(action_space, state, Q)
+            action = agent_move(action_space, state, Q, explore)
             (next_state, agent_reward, done, info) = env.step(action)
             agent_moved = True
             old_value = Q[state, action]
@@ -67,8 +85,8 @@ def play_one(env, Q, opponent, render=False, update=True, first=True):
         if update and agent_moved:
             agent_reward -= opponent_reward
             next_value = np.max(Q[next_state])
-            temp_diff = agent_reward + gamma * next_value
-            Q[state, action] = old_value + alpha * (temp_diff - old_value)
+            temp_diff = agent_reward + gamma * next_value - old_value
+            Q[state, action] = old_value + alpha * temp_diff
         state = next_state
 
     # Game finished
@@ -83,11 +101,53 @@ def play_one(env, Q, opponent, render=False, update=True, first=True):
     return (Q, outcome)
 
 
+def train(epochs, env, Q, opponents):
+    is_first = True
+    for i in range(epochs):
+        (Q, _) = play_one(env, Q, random.choice(opponents), first=is_first)
+        is_first = not is_first
+
+    return Q
+
+
+def test(epochs, env, Q, opponent):
+    outcome_list = [None for i in range(epochs)]
+    is_first = True
+    for i in range(epochs):
+        (_, outcome) = play_one(env, Q, opponent,
+                                first=is_first, update=False, explore=False)
+        outcome_list[i] = outcome
+        is_first = not is_first
+
+    wins = sum(1 for i in outcome_list if i == 'win')
+    losses = sum(1 for i in outcome_list if i == 'loss')
+    drawns = sum(1 for i in outcome_list if i == 'drawn')
+
+    return wins, losses, drawns
+
+
+def human_play(env, Q):
+    is_first = True
+    while True:
+        print('New Game: Agent starts') if is_first else print(
+            'New Game: You start')
+        (_, outcome) = play_one(env, Q, opponent_human,
+                                render=True, update=False, first=is_first, explore=False)
+        if outcome == 'win':
+            print('You lost')
+        elif outcome == 'loss':
+            print('You won')
+        else:
+            print('Drawn')
+
+        is_first = not is_first
+
+
 # Hyperparameters
-epsilon = 0.15  # exploration rate
+epsilon = 0.1  # exploration rate
 alpha = 0.1  # learning rate
-gamma = 0.6  # disount factor
-epochs = 100000  # number of games played while training
+gamma = 0.8  # disount factor
+epochs = 500000  # number of games played while training
 from_scratch = False
 
 
@@ -96,42 +156,22 @@ def main():
     env = gym.make('gym_tictactoe:tictactoe-v0')
 
     # create empty Q-Table or preload
-    if from_scratch:
-        Q = np.zeros([env.observation_space.n, int(env.action_space.n / 2)])
-    else:
-        try:
-            print('Loading Q-Table')
-            Q = pickle.load(open("q_table.p", "rb"))
-        except IOError:
-            print('Could not find file. Starting from scratch')
-            Q = np.zeros([env.observation_space.n,
-                          int(env.action_space.n / 2)])
+    Q = create_Q(env)
 
     # Play against a random player and learn
     print(f'Learning for {epochs} epochs')
-    for i in range(int(epochs)):
-        (Q, _) = play_one(env, Q, opponent_random, first=True)
-        (Q, _) = play_one(env, Q, opponent_random, first=False)
+    Q = train(epochs, env, Q, [opponent_random, opponent_random_better])
     print(f'Finished!')
-
     print('Saving Q-Table')
     pickle.dump(Q, open("q_table.p", "wb"))
 
+    # Test
+    print(f'Testing for {int(epochs/10)} epochs')
+    wins, losses, drawns = test(int(epochs/10), env, Q, opponent_random_better)
+    print(f'Wins: {wins}, Losses: {losses}, Drawns: {drawns}')
+
     # Play against human player
-    global epsilon
-    epsilon = 0  # do not explore
-    is_first = True
-    while True:
-        print('New Game: Agent starts') if is_first else print(
-            'New Game: You start')
-        (_, outcome) = play_one(env, Q, opponent_human,
-                                render=True, update=False, first=is_first)
-        if outcome == 'win':
-            print('You lost')
-        elif outcome == 'loss':
-            print('You won')
-        else:
-            print('Drawn')
+    human_play(env, Q)
 
 
 main()
